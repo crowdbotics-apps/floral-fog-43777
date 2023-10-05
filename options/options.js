90539d07ed10636a94eb4206a931740408de09b7
Crowdbotics <team@crowdbotics.com>
2023-10-05 22:05:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-fog-43777",
+  url: "https://floral-fog-43777.botics.co",
+  api: "https://floral-fog-43777.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
